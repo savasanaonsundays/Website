251e11d16430f0d8723a63fa3a84d415efc6149a
ChristopherForrest <Christopherforrest92@gmail.com>
2018-07-12 15:16:57 +1000

    add contact with mlab

--- back-end/server.js
+++ back-end/server.js
@@ -1,37 +1,66 @@
 const mongoose = require('mongoose');
 const express = require('express');
-
+var seeder = require('mongoose-seed');
 const bookings = require('./routes/api/bookings');
-const content = require('./routes/api/content');
+
 const admin = require('./routes/api/admin');
-
-
-const app = express();
-
-
-//User routes
-
-app.use('/api/admin' , admin);
-app.use('/api/content', content);
-app.use('/api/bookings', bookings);
-
-
+const test = require('./models/test')
+const content = require('./models/Content');
+// Port
 const port = 7000;
-
-
-
+const app = express();
 //DB Config
 const db = require('./config/keys').mongoURI;
 
-//connect to mongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+
+
+// Routes
+
+app.use('/api/admin' , admin);
+
+app.use('/api/bookings', bookings);
+
+
+
+
+
+
+
+// RETURNS ABOUT ME
+
+mongoose.connect(db)
+const aboutMe = mongoose.model("content")
+aboutMe.findOne({name:"aboutMe"}).then(stuff => {
+  console.log(stuff.text)
+})
+
+// RETURNS QUALIFICATIONS
+
+mongoose.connect(db)
+const qualifications = mongoose.model("content")
+qualifications.findOne({name:"qualifications"}).then(stuff => {
+  console.log(stuff.text)
+})
+
+// RETURNS PRICES
+
+mongoose.connect(db)
+const prices = mongoose.model("content")
+prices.findOne({name:"prices"}).then(stuff => {
+  console.log(stuff.text)
+})
+
+
+
+
+
+
 
 
 app.get('/', (req, res) => res.send('Hello'));
 app.listen(port, () => console.log(`server is running on  port ${port}`));
+
+
 
 
 
