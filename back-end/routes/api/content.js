872cf77ca6a7e37db472a7779e318b45429dad9c
ChristopherForrest <Christopherforrest92@gmail.com>
2018-07-12 16:35:06 +1000

    add route

--- back-end/routes/api/content.js
+++ back-end/routes/api/content.js
@@ -1,11 +1,22 @@
 // Different page content routes
-
+const mongoose = require('mongoose');
+const db = require('../../config/keys').mongoURI;
 //  e.g 
 // 
+
 
 const express = require('express')
 const router = express.Router();
 
 router.get('/test', (req,res) => res.json({msg: "content works"}));
 
+router.get('/aboutMe',(req,res) => {
+    mongoose.connect(db)
+    const aboutMe = mongoose.model("content")
+    aboutMe.findOne({name:"aboutMe"}).then(stuff => {
+    res.send(stuff.text)
+})
+})
+
 module.exports = router;
+
