f8c98a2e0b0b04c3cfe3ded790b9364a6c8616fe
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-07-16 11:07:33 +1000

    Added React Router

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -25,12 +25,22 @@
 import React from 'react';
 import './App.css';
 import Springboard from './Components/Springboard';
+import Sometext from './Components/Description';
+import Navbar from './Components/Navbar';
+import Social from './Components/Social';
 
 const App = () => {
   return (
-    <div className="App">
-      <Springboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <Sometext />
+        <Springboard />
+
+        <Social />
+      </div>
+    </Router>
   );
 };
 
