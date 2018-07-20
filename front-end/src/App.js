2c0dcf56cf01519fbb0625aa570d899fc9e6aec0
elouise <elouise.l.pollock@gmail.com>
2018-07-20 18:30:17 +1000

    The rest of the owl

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
+import Register from './Components/auth/Register';
+import Login from './Components/auth/Login';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentAdmin, logoutAdmin } from './actions/authActions';
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Springboard from './Components/Springboard';
 import Landing from './Components/Description';
 import Navbar from './Components/Navbar';
@@ -14,27 +22,44 @@ import Qualifications from './Components/Qualifications';
 import Hero from './Components/Hero';
 import RealHero from './Components/RealHero';
 
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentAdmin(decoded));
+
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutAdmin());
+    // window.location.href = '/login'
+  }
+}
+
 const App = () => (
-  <Router>
-    <div className="App">
-      <Route exact path="/" component={RealHero} />
-      <Route exact path="/" component={Springboard} />
-      <Route exact path="/aboutme" className="notHome" component={Navbar} />
-      <Route
-        exact
-        path="/qualifications"
-        className="notHome"
-        component={Navbar}
-      />
-      <Route exact path="/prices" className="notHome" component={Navbar} />
+  <Provider store={store}>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={RealHero} />
+        <Route exact path="/" component={Springboard} />
+        <Route exact path="/aboutme" className="notHome" component={Navbar} />
+        <Route
+          exact
+          path="/qualifications"
+          className="notHome"
+          component={Navbar}
+        />
+        <Route exact path="/prices" className="notHome" component={Navbar} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
 
-      <Route exact path="/aboutme" component={Aboutme} />
-      <Route exact path="/qualifications" component={Qualifications} />
-      <Route exact path="/pricing" component={Prices} />
+        <Route exact path="/aboutme" component={Aboutme} />
+        <Route exact path="/qualifications" component={Qualifications} />
+        <Route exact path="/pricing" component={Prices} />
 
-      <Footer />
-    </div>
-  </Router>
+        <Footer />
+      </div>
+    </Router>
+  </Provider>
 );
 
 export default App;
